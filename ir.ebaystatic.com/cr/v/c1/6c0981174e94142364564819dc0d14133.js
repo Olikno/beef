94d719de2dc6e8a68778e1aace67580bd0ac5f73
Olikno <101351813+Olikno@users.noreply.github.com>
2024-04-15 20:33:19 +0100

    Add files via upload

---
+++ ir.ebaystatic.com/cr/v/c1/6c0981174e94142364564819dc0d14133.js
@@ -0,0 +1,96 @@
+
+GH_alertTemplate({
+    "iLink" : "https://www.ebay.co.uk/itm/#ITEMID#?_ilink",
+    "OUTBIDl": "https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l5997",
+    "OUTBIDlp": "https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6026",
+    "OUTBIDic": "<i class='ghn-ic-ob'/></i>",
+    "BIDITEMl": "https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l5998",
+    "BIDITEMlp": "https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6027",
+    "WATCHITMl": "https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l5999",
+    "WATCHITMlp": "https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6028",
+    "SHOPCARTITMl":"https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6000",
+    "SHOPCARTITMlp":"https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6029",
+    "ITMWONl": "https://pay.ebay.co.uk/xo?action=create&item=#ITEMID##TRID#&_trksid=m570.l6001",
+    "ITMWONlp": "https://pay.ebay.co.uk/xo?action=create&item=#ITEMID##TRID#&_trksid=m570.l6030",
+    "ITMSOLDl": "https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6002",
+    "ITMSOLDlp": "https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6031",
+
+    "COCMPLTl": "https://www.ebay.co.uk/lbr/go?t=#ITEMID##TRID_e#&_trksid=m570.l6003",
+    "COCMPLTlp": "https://www.ebay.co.uk/lbr/go?t=#ITEMID##TRID_e#&_trksid=m570.l6032",
+
+    "ITMSHPDl": "https://www.ebay.co.uk/vod/FetchOrderDetails?ViewPaymentStatus#TRID#&itemId=#ITEMID#&_trksid=m570.l6004",
+    "ITMSHPDlp": "https://www.ebay.co.uk/vod/FetchOrderDetails?ViewPaymentStatus#TRID#&itemId=#ITEMID#&_trksid=m570.l6033",
+    "BESTOFRl": "https://offer.ebay.co.uk/ws/eBayISAPI.dll?ManageBestOffers&itemId=#ITEMID#&_trksid=m570.l6005",
+    "BESTOFRlp": "https://offer.ebay.co.uk/ws/eBayISAPI.dll?ManageBestOffers&itemId=#ITEMID#&_trksid=m570.l6034",
+    "BODECLNDl": "https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6006&boolp=1",
+    "BODECLNDlp": "https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6035&boolp=1",
+
+    "CNTROFFRl": "https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6007&boolp=1",
+    "CNTROFFRlp": "https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6036&boolp=1",
+
+    "CNTROFFRl_B": "https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6345&boolp=1",
+    "CNTROFFRlp_B": "https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6346&boolp=1",
+
+    "CNTROFFRl_S": "https://offer.ebay.co.uk/ws/eBayISAPI.dll?ManageBestOffers&itemId=#ITEMID#&_trksid=m570.l6339",
+    "CNTROFFRlp_S": "https://offer.ebay.co.uk/ws/eBayISAPI.dll?ManageBestOffers&itemId=#ITEMID#&_trksid=m570.l6340",
+
+    "BOACCPTDl" :  "https://pay.ebay.co.uk/xo?action=create&item=#ITEMID##TRID#&_trksid=m570.l6019",
+    "BOACCPTDlp" : "https://pay.ebay.co.uk/xo?action=create&item=#ITEMID##TRID#&_trksid=m570.l6037",
+
+    "COACCPTEDl":"https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6020",
+    "COACCPTEDlp":"https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6038",
+
+    "COACCPTEDl_S":"https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6347",
+    "COACCPTEDlp_S":"https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6348",
+
+    "COACCPTEDl_B":"https://pay.ebay.co.uk/xo?action=create&item=#ITEMID##TRID#&_trksid=m570.l6343",
+    "COACCPTEDlp_B":"https://pay.ebay.co.uk/xo?action=create&item=#ITEMID##TRID#&_trksid=m570.l6344",
+
+    "CODECLNDl":"https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6021&boolp=1",
+    "CODECLNDlp":"https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6039&boolp=1",
+
+    "CODECLNDl_B":"https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6349&boolp=1",
+    "CODECLNDlp_B":"https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6350&boolp=1",
+
+    "CODECLNDl_S":"https://offer.ebay.co.uk/ws/eBayISAPI.dll?ManageBestOffers&itemId=#ITEMID#&_trksid=m570.l6341",
+    "CODECLNDlp_S":"https://offer.ebay.co.uk/ws/eBayISAPI.dll?ManageBestOffers&itemId=#ITEMID#&_trksid=m570.l6342",
+
+    "SellerInitiatedOfferl": "https://www.ebay.co.uk/ulk/offerapp/sio/acceptOffer/#ITEMID#/#OFFERID#?_trksid=m570.l47333",
+    "SellerInitiatedOfferlp": "https://www.ebay.co.uk/ulk/offerapp/sio/acceptOffer/#ITEMID#/#OFFERID#?_trksid=m570.l47333",
+
+    "SellerInitiatedOfferl_S": "https://www.ebay.co.uk/ulk/sio/#ITEMID#/#OFFERID#/#NEGOID#?_trksid=m570.l47448",
+    "SellerInitiatedOfferlp_S": "https://www.ebay.co.uk/ulk/sio/#ITEMID#/#OFFERID#/#NEGOID#?_trksid=m570.l47448",
+
+    "OFREXPIREDl":"https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6022",
+    "OFREXPIREDlp":"https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6040",
+    "OFRDCLNDACPTl":"https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6023",
+    "OFRDCLNDACPTlp":"https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6041",
+    "OFRRETRACTEDl":"https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6024",
+    "OFRRETRACTEDlp":"https://www.ebay.co.uk/itm/#ITEMID#?_trksid=m570.l6042",
+    "MSGM2MMSGHDRl":"https://mesg.ebay.co.uk/mesgweb/ViewMessageDetail/0/All/#MSGID#?_trksid=m570.l6025",
+    "MSGM2MMSGHDRlp":"https://mesg.ebay.co.uk/mesgweb/ViewMessageDetail/0/All/#MSGID#?_trksid=m570.l6043",
+    "UCIl": "https://accountsettings.ebay.co.uk/uci?ft=NOTIFY",
+    "UCIlp": "https://accountsettings.ebay.co.uk/uci?ft=NOTIFY",
+    "1":"https://www.ebay.co.uk/myb/PurchaseHistory?_tkrsid=m570.I6828",
+    "1p":"https://www.ebay.co.uk/myb/PurchaseHistory?tkrsid=m570.I6832",
+
+    "3": "https://www.ebay.co.uk/lbr/go?_trksid=m570.l6829",
+    "3p": "https://www.ebay.co.uk/lbr/go?_trksid=m570.l6833",
+    "6":"https://mesg.ebay.co.uk/mesgweb/ViewMessages/0?_trksid=m570.l6830",
+    "6p":"https://mesg.ebay.co.uk/mesgweb/ViewMessages/0?_trksid=m570.l6834",
+    "7":"https://www.ebay.co.uk/myb/PurchaseHistory?_tkrsid=m570.I6831",
+    "7p":"https://www.ebay.co.uk/myb/PurchaseHistory?_tkrsid=m570.I6835",
+
+    "s0h"  : "<span class='ghn-cd ghn-hl'  >#DIFF#</span>",
+    "tlf":{
+        "d":"d",
+        "h":"h",
+        "m":"m",
+        "s":"s",
+        "e":"Ended"
+    },
+    "tmpl_i" : "<li data-id='#ITEMID#' data-uid='#UID#' class='ghn-li-itm #NEW# #TYPE# #UI# #TOUCH#' #P# ><a class='ghn-a-itm' href='#LINK#'><div class='ghn-pop-new' tabindex='0' aria-label='#newNotif#'><i class='ghn-new-ic'></i></div><div class='ghn-a-img'><div class='ghn-div-img'><img class=ghn-i src='#imgsrc#'/>#H#</div>#IC#</div><div class='ghn-desc'><span class='ghn-desc-t'>#L10N_t#</span><span class='ghn-desc-d'>#DIFF#</span><span class='ghn-desc-o'>#L10N_s#</span><span class='ghn-desc-a'>#L10N_a#</span></div></a><button aria-label='#rmv#' class='ghn-pop-rmv'><i class='ghn-pop-rmv-ic'></i></button></li>",
+    "tmpl_g" : "<li data-id='#ITEMID#' data-uid='#UID#' data-groupid='#GROUPID#' class='ghn-li-itm #NEW# #TYPE# #UI# #TOUCH#' #P#><a class='ghn-a-itm' href='#LINK#'><div class='ghn-pop-new' tabindex='0' aria-label='#newNotif#'><i class='ghn-new-ic'></i></div><div class='ghn-a-img'><div class='ghn-div-img'><img class=ghn-i src='#imgsrc#' />#H#<div class='ghn-bmi'></div><div class='ghn-g-num-c'><span class='ghn-g-num'>#NUM#</span></div></div>#IC#</div><div class='ghn-desc'><span class='ghn-desc-t'>#L10N_t# </span><span class='ghn-desc-d'>#DIFF#</span><span class='ghn-desc-o ghellip'>#L10N_s#</span><span class='ghn-desc-a'>#L10N_a#</span></div></a><button aria-label='#rmv#' class='ghn-pop-rmv'><i class='ghn-pop-rmv-ic'></i></button></li>",
+    "tmpl_p" : "<div id='ghn-pop-#UID#' data-id='#ITEMID#' data-uid='#UID#' class='ghn-pop-i #TYPE# #UI# #TOUCH#'><div class='ghn-li-itm'><a class='ghn-a-itm' href='#LINKP#'><div class='ghn-pop-new' tabindex='0' aria-label='#newNotif#'><i class='ghn-new-ic'></i></div><div class='ghn-a-img'><div class='ghn-div-img'><img class=ghn-i src='#imgsrc#'/>#H#</div>#IC#</div><div class='ghn-desc'><span class='ghn-desc-t' style=''>#L10N_t# </span><span class='ghn-desc-d'>#DIFF#</span><span class='ghn-desc-o'>#L10N_s#</span><span class='ghn-desc-a'>#L10N_a#</span></div></a><button aria-label='#rmv#' class='ghn-pop-rmv'><i class='ghn-pop-rmv-ic'></i></button></div></div>",
+    "tmpl_error":  "<div id='ghn-h'>Notifications</div><div id='ghn-err'><span class='ghn-errb'>#TEXT#</span></div><div id='ghn-f'></div>"
+})
